10f0610107a05e6bd875a0ded0ee35fcb1b74863
Dharmendra Singh <dharmenn@thoughtworks.com>
2015-10-25 21:59:36 +0530

    added an event function to the try.js

--- try.js
+++ try.js
@@ -1,3 +1,17 @@
-gf kjgdfb
-df bdkjfjkd
-lbjkdf lbjkdf
+var events = require('events').EventEmitter;
+var eventEmitter = new events();
+var name = process.argv.slice(2);
+name = name.join(' ').toLowerCase()
+var hello = ["dhar","∂har"].join('|')
+var pattern = new RegExp(hello,"gi")
+var callfun = function ringBell()
+{
+	if(name&&name.match(pattern))
+		console.log("kutte bade bhai se ungli nahi karte")
+  	if(name&&!name.match(pattern))console.log(name +" bachha hai tu abhi");
+  	if(!name) console.log("kuch input to de bhai")
+}
+
+
+eventEmitter.on(name, callfun);
+eventEmitter.emit(name);
